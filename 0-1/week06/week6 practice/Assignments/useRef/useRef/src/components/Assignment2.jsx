81a5d3b9998555e1af06a578c1428ff36604ac43
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2025-05-08 22:57:16 +0530

    Add files via upload

--- 0-1/week06/week6 practice/Assignments/useRef/useRef/src/components/Assignment2.jsx
+++ 0-1/week06/week6 practice/Assignments/useRef/useRef/src/components/Assignment2.jsx
@@ -3,6 +3,7 @@ import React, { useState, useCallback, useRef } from 'react';
 // Create a component that tracks and displays the number of times it has been rendered. Use useRef to
 // create a variable that persists across renders without causing additional renders when it changes.
 
+// let noOfCounts=0
 export function Assignment2() {
   const [count, setCount] = useState(0);
 
@@ -15,6 +16,9 @@ export function Assignment2() {
     // Increment forceRender.current when the button is clicked
   };
   forceRender.current = forceRender.current + 1;
+
+  // noOfCounts++;
+
 
   return (
     <div>
